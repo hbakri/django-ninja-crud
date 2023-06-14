8dd090350bea83b7948e782a74d350c5b10b1f8f
hbakri-picsellia <hicham.bakri@picsellia.com>
2023-06-14 13:43:05 +0200

    minor fixes

--- ninja_crud/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Django Ninja CRUD - Fast Django REST framework for building CRUD APIs with Django Ninja."""
-
-__version__ = "0.1.0"
-__author__ = "Hicham Bakri"
-__email__ = "hicham.bakri76@gmail.com"
-__license__ = "MIT"
-__description__ = "Fast Django REST framework for building CRUD APIs with Django Ninja"
