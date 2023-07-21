6b48f351cceb469440a361542fba892e85a4f26a
Hicham Bakri <92298226+hbakri@users.noreply.github.com>
2023-07-21 08:40:48 +0200

    refactor: split `AbstractModelViewTest` and `ModelViewSetTest` class into separate files (#54)

--- ninja_crud/tests/__init__.py
+++ ninja_crud/tests/__init__.py
@@ -1,11 +1,12 @@
 from .request_components import AuthHeaders, PathParameters, Payloads, QueryParameters
 from .request_composer import RequestComposer
-from .test_abstract import AbstractModelViewTest, ModelViewSetTest
+from .test_abstract import AbstractModelViewTest
 from .test_create import CreateModelViewTest
 from .test_delete import DeleteModelViewTest
 from .test_list import ListModelViewTest
 from .test_retrieve import RetrieveModelViewTest
 from .test_update import UpdateModelViewTest
+from .test_viewset import ModelViewSetTest
 
 __all__ = [
     "AbstractModelViewTest",
--- ninja_crud/tests/test_abstract.py
+++ ninja_crud/tests/test_abstract.py
@@ -3,12 +3,7 @@ from typing import Callable, List, Tuple, Type
 
 from django.test import Client, TestCase
 
-from ninja_crud.views import (
-    AbstractModelView,
-    CreateModelView,
-    ListModelView,
-    ModelViewSet,
-)
+from ninja_crud.views import AbstractModelView, ModelViewSet
 
 
 class AbstractModelViewTest:
@@ -34,39 +29,3 @@ class AbstractModelViewTest:
                 and self.name == f"test_{attr_name}"
             ):
                 return attr_value
-
-
-class ModelViewSetTestMeta(type):
-    model_view_set_class: ModelViewSet
-    urls_prefix: str
-    client_class: Callable[[], Client]
-
-    def __new__(mcs, name, bases, dct):
-        new_cls = super().__new__(mcs, name, bases, dct)
-        test_case = new_cls()
-        for attr_name in dir(new_cls):
-            attr_value = getattr(new_cls, attr_name)
-            if isinstance(attr_value, AbstractModelViewTest):
-                attr_value.model_view_set = new_cls.model_view_set_class
-                attr_value.urls_prefix = new_cls.urls_prefix
-                attr_value.test_case = test_case
-                attr_value.client = new_cls.client_class()
-                attr_value.name = attr_name
-                for test_name, test_func in attr_value.get_test_methods():
-                    model_view = attr_value.get_model_view()
-                    model_name = new_cls.model_view_set_class.model.__name__.lower()
-                    substring_replace = model_name
-                    if (
-                        isinstance(model_view, (ListModelView, CreateModelView))
-                        and model_view.detail
-                    ):
-                        related_model_name = model_view.related_model.__name__.lower()
-                        substring_replace = f"{model_name}_{related_model_name}"
-                    new_test_name = test_name.replace("model", substring_replace)
-                    setattr(new_cls, new_test_name, test_func)
-        return new_cls
-
-
-class ModelViewSetTest(metaclass=ModelViewSetTestMeta):
-    model_view_set_class: ModelViewSet
-    urls_prefix: str
