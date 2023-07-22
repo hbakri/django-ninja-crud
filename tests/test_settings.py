be9dc9c4602b12431227bd3553b6ca5da92b2538
Hicham Bakri <92298226+hbakri@users.noreply.github.com>
2023-07-22 15:12:02 +0200

    chore: add `LOGGING` configuration in test settings (#63)

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -12,3 +12,29 @@ DATABASES = {
     }
 }
 ROOT_URLCONF = "tests.test_urls"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        "ninja_crud": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+            "propagate": False,
+        },
+    },
+}
