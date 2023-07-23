4814dd3bd5d0ccfbf5e9d81276fa554d0203b2d4
Hicham Bakri <92298226+hbakri@users.noreply.github.com>
2023-07-23 09:39:55 +0200

    feat: enforce `ModelViewSetTest` subclasses to be also `TestCase` subclasses (#73)

--- ninja_crud/tests/test_viewset.py
+++ ninja_crud/tests/test_viewset.py
@@ -1,4 +1,6 @@
-from typing import Type
+from typing import Type, Union
+
+from django.test import TestCase
 
 from ninja_crud.tests.test_abstract import AbstractModelViewTest
 from ninja_crud.tests.test_matcher import ModelViewSetTestMatcher
@@ -6,29 +8,38 @@ from ninja_crud.views import CreateModelView, ListModelView, ModelViewSet
 
 
 class ModelViewSetTestMeta(type):
-    model_view_set_class: Type[ModelViewSet]
-    urls_prefix: str
-
     def __new__(mcs, name, bases, dct):
         new_cls = super().__new__(mcs, name, bases, dct)
-        test_case = new_cls()
+
+        if name == "ModelViewSetTest":
+            return new_cls
+        elif not issubclass(new_cls, TestCase):
+            raise TypeError(
+                f"{new_cls.__name__} must inherit from both ModelViewSetTest and django.test.TestCase"
+            )  # pragma: no cover
+
+        model_view_set_test: Union[ModelViewSetTest, TestCase] = new_cls()
         associated_model_views = []
         for attr_name in dir(new_cls):
             attr_value = getattr(new_cls, attr_name)
             if isinstance(attr_value, AbstractModelViewTest):
-                attr_value.model_view_set_class = new_cls.model_view_set_class
-                attr_value.urls_prefix = new_cls.urls_prefix
-                attr_value.test_case = test_case
+                attr_value.model_view_set_class = (
+                    model_view_set_test.model_view_set_class
+                )
+                attr_value.urls_prefix = model_view_set_test.urls_prefix
+                attr_value.test_case = model_view_set_test
                 attr_value.model_view = (
                     ModelViewSetTestMatcher.get_associated_model_view(
-                        model_view_set_class=new_cls.model_view_set_class,
+                        model_view_set_class=model_view_set_test.model_view_set_class,
                         test_attr_name=attr_name,
                         test_attr_value=attr_value,
                     )
                 )
                 associated_model_views.append(attr_value.model_view)
                 for test_name, test_func in attr_value.get_test_methods():
-                    model_name = new_cls.model_view_set_class.model.__name__.lower()
+                    model_name = (
+                        model_view_set_test.model_view_set_class.model.__name__.lower()
+                    )
                     substring_replace = model_name
                     if (
                         isinstance(
