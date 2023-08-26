398572bf9d45e2117b494e0b8a7411acba7c6160
Hicham Bakri <92298226+hbakri@users.noreply.github.com>
2023-08-26 22:28:09 +0200

    fix: markdown `filenames` in GitHub `Wiki` (#203)

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -3,35 +3,36 @@ import subprocess
 
 import yaml
 
+root_dir = os.path.abspath(os.path.join(os.getcwd()))
 project_name = "ninja_crud"
 modules = [
-    "views.abstract",
-    "views.retrieve",
-    "views.update",
-    "views.partial_update",
-    "views.delete",
-    "views.list",
-    "views.create",
-    "views.types",
-    "views.viewset",
+    ("views/abstract", "AbstractModelView"),
+    ("views/retrieve", "RetrieveModelView"),
+    ("views/update", "UpdateModelView"),
+    ("views/partial_update", "PartialUpdateModelView"),
+    ("views/delete", "DeleteModelView"),
+    ("views/list", "ListModelView"),
+    ("views/create", "CreateModelView"),
+    ("views/types", "Types"),
+    ("views/viewset", "ModelViewSet"),
 ]
 
 template_path = "docs/pydoc-markdown.yml"
 with open(template_path, "r") as file:
     template = yaml.load(file, Loader=yaml.FullLoader)
 
-root_dir = os.path.abspath(os.path.join(os.getcwd()))
-
-for module in modules:
-    output_path = f"docs/reference/{module.replace('.', '/')}.md"
+for module_path, module_title in modules:
+    reference_dir = "docs/reference"
+    module_dir = "/".join(module_path.split("/")[:-1])
+    output_path = os.path.join(reference_dir, module_dir, f"{module_title}.md")
     template["renderer"]["filename"] = output_path
+    yaml_config_str = yaml.dump(template)
 
     # Create necessary directories if they don't exist
     output_dir = os.path.dirname(output_path)
     os.makedirs(output_dir, exist_ok=True)
 
-    module_path = os.path.join(root_dir, f"{project_name}/{module.replace('.', '/')}")
-    yaml_config_str = yaml.dump(template)
+    module_absolute_path = os.path.join(root_dir, project_name, module_path)
     subprocess.run(
-        ["poetry", "run", "pydoc-markdown", "-m", module_path, yaml_config_str]
+        ["poetry", "run", "pydoc-markdown", "-m", module_absolute_path, yaml_config_str]
     )
