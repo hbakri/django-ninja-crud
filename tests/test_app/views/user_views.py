649ac78082d39334fa3924dd9f7466926ce5993f
Hicham Bakri <hicham.bakri@picsellia.com>
2023-12-25 01:37:32 +0100

    fix: test_app UserViewSet

--- tests/test_app/views/user_views.py
+++ tests/test_app/views/user_views.py
@@ -1,13 +1,7 @@
 from django.contrib.auth.models import User
 from ninja import Router
 
-from ninja_crud.views import (
-    CreateModelView,
-    DeleteModelView,
-    ListModelView,
-    RetrieveModelView,
-    UpdateModelView,
-)
+from ninja_crud import views
 from ninja_crud.viewsets import ModelViewSet
 from tests.test_app.schemas import UserIn, UserOut
 
@@ -17,11 +11,11 @@ router = Router()
 class UserViewSet(ModelViewSet):
     model = User
 
-    list_users = ListModelView(output_schema=UserOut, pagination_class=None)
-    create_user = CreateModelView(input_schema=UserIn, output_schema=UserOut)
-    retrieve_user = RetrieveModelView(output_schema=UserOut)
-    update_user = UpdateModelView(input_schema=UserIn, output_schema=UserOut)
-    delete_user = DeleteModelView()
+    list_users = views.ListModelView(output_schema=UserOut, pagination_class=None)
+    create_user = views.CreateModelView(input_schema=UserIn, output_schema=UserOut)
+    retrieve_user = views.RetrieveModelView(output_schema=UserOut)
+    update_user = views.UpdateModelView(input_schema=UserIn, output_schema=UserOut)
+    delete_user = views.DeleteModelView()
 
 
 UserViewSet.register_routes(router)
