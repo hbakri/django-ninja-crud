127250b19a5a60cc222288f16eb84609e1fbbe6b
Hicham Bakri <hicham.bakri@picsellia.com>
2024-10-13 15:33:49 +0200

    fix: remove async types for now

--- ninja_crud/views/types.py
+++ ninja_crud/views/types.py
@@ -1,13 +1,3 @@
-from typing import Any, Awaitable, Callable, Optional
-
-from django.db.models import Model
-from django.http import HttpRequest
-from pydantic import BaseModel
+from typing import Any, Callable
 
 Decorator = Callable[[Callable[..., Any]], Callable[..., Any]]
-
-GetModel = Callable[[HttpRequest, Optional[BaseModel]], Model]
-AsyncGetModel = Callable[[HttpRequest, Optional[BaseModel]], Awaitable[Model]]
-
-ModelHook = Callable[[HttpRequest, Model], None]
-AsyncModelHook = Callable[[HttpRequest, Model], Awaitable[None]]
