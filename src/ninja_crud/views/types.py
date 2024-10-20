5398f93653a19bc9c0d81c3430de14af2b0d898c
Hicham Bakri <hicham.bakri@picsellia.com>
2024-10-20 13:23:29 +0200

    fix: avoid pipe for optional type aliases for now

--- src/ninja_crud/views/types.py
+++ src/ninja_crud/views/types.py
@@ -1,5 +1,5 @@
 from collections.abc import Awaitable
-from typing import Any, Callable
+from typing import Any, Callable, Optional
 
 from django.db.models import Model, QuerySet
 from django.http import HttpRequest
@@ -10,11 +10,11 @@ Decorator = Callable[[Callable[..., Any]], Callable[..., Any]]
 ModelHook = Callable[[HttpRequest, Model], None]
 AsyncModelHook = Callable[[HttpRequest, Model], Awaitable[None]]
 
-ModelGetter = Callable[[HttpRequest, BaseModel | None], Model]
-AsyncModelGetter = Callable[[HttpRequest, BaseModel | None], Awaitable[Model]]
+ModelGetter = Callable[[HttpRequest, Optional[BaseModel]], Model]
+AsyncModelGetter = Callable[[HttpRequest, Optional[BaseModel]], Awaitable[Model]]
 
-QuerySetGetter = Callable[[HttpRequest, BaseModel | None], QuerySet[Model]]
+QuerySetGetter = Callable[[HttpRequest, Optional[BaseModel]], QuerySet[Model]]
 AsyncQuerySetGetter = Callable[
-    [HttpRequest, BaseModel | None], Awaitable[QuerySet[Model]]
+    [HttpRequest, Optional[BaseModel]], Awaitable[QuerySet[Model]]
 ]
-QuerySetFilter = Callable[[QuerySet[Model], BaseModel | None], QuerySet[Model]]
+QuerySetFilter = Callable[[QuerySet[Model], Optional[BaseModel]], QuerySet[Model]]
