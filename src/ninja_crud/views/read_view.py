cfdf6fe0aeae4c1f62147cc0467f3fe9ada96a36
Ksauder <ksauder.dev@gmail.com>
2025-08-14 20:26:57 -0400

    change path default to "/{int:id}"

--- src/ninja_crud/views/read_view.py
+++ src/ninja_crud/views/read_view.py
@@ -75,7 +75,7 @@ class ReadView(APIView):
         self,
         name: Optional[str] = None,
         methods: Union[list[str], set[str], None] = None,
-        path: str = "/{id}",
+        path: str = "/{int:id}",
         response_status: int = 200,
         response_body: Any = None,
         model: Optional[type[Model]] = None,
